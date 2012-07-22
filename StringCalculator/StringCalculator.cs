550f68a088bd202404867adc8809b65fb8228f1b
Carlos Durban Antuñano <kaken20@gmail.com>
2012-07-22 20:45:03 +0200

    Commit

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        protected string NumbersString { get; set; }
+        protected string[] NumbersArray { get; set; }
+
+        public int Add(string numbers)
+        {
+            this.NumbersString = numbers;
+
+            if (IsEmptyNumbers())
+                return 0;
+
+            SplitNumberStringInArray();
+
+            CheckIfExistNegativeNumbers();
+
+            RemoveNumbersBigThan1000();
+
+            return NumbersArray.Sum(x => int.Parse(x));
+
+        }
+
+        private bool IsEmptyNumbers()
+        {
+            return string.IsNullOrEmpty(NumbersString);
+        }
+
+        private void SplitNumberStringInArray()
+        {
+            string[] Delimiters = ConfigureDelimiters();
+
+            Regex f = new Regex(@"(\d+)");
+
+            NumbersArray = f.Split(NumbersString);
+
+            NumbersArray = NumbersString.Split(Delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private string[] ConfigureDelimiters()
+        {
+            //Char[] Delimiters = { ',', Convert.ToChar('\n') };
+            List<string> Delimiters = new List<string>();
+            Delimiters.Add(",");
+            Delimiters.Add("\n");
+
+            Regex regularExpresion = new Regex(@"(?<=\[)(.*?)(?=\])");
+            var matches = regularExpresion.Matches(NumbersString);
+
+            foreach (Match mach in matches)
+                Delimiters.Add(mach.Value);
+            
+
+
+            //if (NumbersString.StartsWith("//"))
+            //{
+            //    Delimiters[0] = Convert.ToChar(NumbersString.Substring(2, 1));
+            //    NumbersString = NumbersString.Remove(0, 2);
+            //}
+
+            return Delimiters.ToArray();
+        }
+
+        protected void CheckIfExistNegativeNumbers()
+        {
+            var numbersNegative = NumbersArray.Where(x => int.Parse(x) < 0);
+            if (numbersNegative.Any())
+                throw new ApplicationException("Negatives not allowed: " + string.Join(", ", numbersNegative.ToArray()));
+            
+        }
+
+        protected void RemoveNumbersBigThan1000()
+        {
+            var num = NumbersArray.ToList();
+            num.RemoveAll(x => int.Parse(x) > 1000);
+            NumbersArray = num.ToArray();
+        }
+    }
+}
---
+++ StringCalculatorTest/StringCalculatorTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using StringCalculator;
+using NUnit;
+using NUnit.Framework;
+
+namespace StringCalculatorTest
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+
+        [Test]
+        public void ForStringEmptyReturnZero()
+        {
+            string numbers = "";
+
+            Assert.AreEqual(0, new StringCalculator.StringCalculator().Add(numbers));
+
+        }
+
+        [Test]
+        public void AddOneNumberReturnThis()
+        {
+            string numbers = "1";
+
+            Assert.AreEqual(1, new StringCalculator.StringCalculator().Add(numbers));
+        }
+        
+        [Test]
+        public void AddNumbersReturnSum()
+        {
+            string numbers = "1, 0, 2";
+
+            Assert.AreEqual(3, new StringCalculator.StringCalculator().Add(numbers));
+        }
+
+        [Test]
+        public void AddNewLinesInNumberReturnSum()
+        {
+            string numbers = "1\n0, 2";
+
+            Assert.AreEqual(3, new StringCalculator.StringCalculator().Add(numbers));
+        }
+
+        [Test]
+        public void ChangeDefaultDelimiter()
+        {
+            string numbers = "//;\n1;2";
+
+            Assert.AreEqual(3, new StringCalculator.StringCalculator().Add(numbers));
+        }
+
+        [Test]
+        public void NegativeNumberNotAllowedThrowException()
+        {
+            string numbers = "1,-2";
+            try
+            {
+                new StringCalculator.StringCalculator().Add(numbers);
+                Assert.Fail("Fail Test, exception expected");
+            }
+            catch (ApplicationException ex)
+            {
+                Assert.AreEqual("Negatives not allowed: -2", ex.Message);
+            }
+        }
+
+        [Test]
+        public void NegativeNumbersNotAllowedThrowException()
+        {
+            string numbers = "1,-2,-3";
+            try
+            {
+                new StringCalculator.StringCalculator().Add(numbers);
+                Assert.Fail("Fail Test, exception expected");
+            }
+            catch (ApplicationException ex)
+            {
+                Assert.AreEqual("Negatives not allowed: -2, -3", ex.Message);
+            }
+        }
+
+        [Test]
+        public void NumbersBigThan1000Ignored()
+        {
+            string numbers = "1,1001";
+
+            Assert.AreEqual(1, new StringCalculator.StringCalculator().Add(numbers));
+        }
+
+        [Test]
+        public void DelimiterCanBeAnyLength()
+        {
+            string numbers = "//[***]\n1***2***3";
+
+            Assert.AreEqual(6, new StringCalculator.StringCalculator().Add(numbers));
+        }
+
+        
+
+    }
+}
